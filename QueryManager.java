841e3d6cf3484fa4dd93a7863c574b6bd465a395
charlesC <charles.chu9@gmail.com>
2025-04-09 04:06:56 -0500

    complete CLI

--- QueryManager.java
+++ QueryManager.java
@@ -7,7 +7,7 @@ public class QueryManager {
     private Connection connection;
 
     // class constant
-    private final String url = "jdbc:sqlite::memory:"; // create databse in memory
+    private final String url = "jdbc:sqlite:f1.db"; // create databse
 
     // Constructor - establish connection to database
     public QueryManager() {
@@ -46,6 +46,33 @@ public class QueryManager {
         } else if (command.equals("h")) { // help command
             this.help();
 
+        } else if (command.equals("drivers")) { // drivers command
+            this.drivers();
+
+        } else if (command.equals("constructors")) { // constructors command
+            this.constructors();
+
+        } else if (command.equals("races")) { // races command
+            this.races();
+
+        } else if (command.equals("team_drivers")) { // team_drivers command
+            this.teamDrivers(variable);
+
+        } else if (command.equals("driver_teams")) { // driver_teams command
+            this.driverTeams(variable);
+
+        } else if (command.equals("circuits")) { // circuits command
+            this.circuits();
+
+        } else if (command.equals("fastest_lap")) { // fastest_lap command
+            this.fastestLap(variable);
+
+        } else if (command.equals("top_drivers")) { // top_drivers command
+            this.topDrivers();
+
+        } else if (command.equals("search_driver")) { // search_driver command
+            this.searchDriver(variable);
+
         } else { // invalid command
             System.out.println("Please enter valid command");
         }
@@ -57,6 +84,70 @@ public class QueryManager {
 
     }
 
+    // TODO: implement query
+    private void drivers() {
+        System.out.println("drivers");
+    }
+
+    // TODO: implement query
+    private void constructors() {
+        System.out.println("constructors");
+    }
+
+    // TODO: implement query
+    private void races() {
+        System.out.println("races");
+    }
+
+    // TODO: implement query
+    private void teamDrivers(String teamName) {
+        if (!teamName.equals("")) {
+            System.out.println("team_drivers");
+        } else {
+            System.out.println("Enter a valid team name (team_drivers <teamName>)");
+        }
+    }
+
+    // TODO: implement query
+    private void driverTeams(String driverName) {
+        if (!driverName.equals("")) {
+            System.out.println("driver_teams");
+        } else {
+            System.out.println("Enter a valid driver name (driver_teams <driverName>)");
+        }
+    }
+
+    // TODO: implement query
+    private void circuits() {
+        System.out.println("circuits");
+    }
+
+    // TODO: implement query
+    private void fastestLap(String driverName) {
+        if (!driverName.equals("")) {
+            System.out.println("fastest_lap");
+        } else {
+            System.out.println("Enter a valid driver name (fastest_lap <driverName>)");
+        }
+    }
+
+    // TODO: implement query
+    private void topDrivers() {
+        System.out.println("top_drivers");
+    }
+
+    // TODO: implement query
+    private void searchDriver(String driverName) {
+        if (!driverName.equals("")) {
+            System.out.println("search_driver");
+        } else {
+            System.out.println("Enter a valid driver name (search_driver <driverName>)");
+        }
+    }
+
+    /**
+     * Prints the help menu
+     */
     private void help() {
         System.out.println("This Program contains information about F1 World Championship from 1950 - 2024");
         System.out.println("\nThe following commands are available in this programm:");
