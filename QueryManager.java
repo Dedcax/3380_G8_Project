0431199e26f33f813d76bdcfcb37411baaf15fd5
charlesC <charles.chu9@gmail.com>
2025-04-09 03:46:32 -0500

    help command

--- QueryManager.java
+++ QueryManager.java
@@ -31,19 +31,49 @@ public class QueryManager {
         String variable = input[InputHandler.VARIABLE];
         boolean continueProgram = true;
 
+        // formating gaps for easy readability
+        System.out.println();
+
         // respond to user input
-        if (command.length() < 1) {
+        if (command.length() < 1) { // invalid inputs
             System.out.println("\nPlease enter valid command");
-        } else if (command.equals("q")) {
+
+        } else if (command.equals("q")) { // quit command
             System.out.println("Thank you for using our program.");
+            System.out.println("Exiting...");
             continueProgram = false;
-        } else if (command.equals("h")) {
-            System.out.println("Welcome to the Help page");
-        } else {
+
+        } else if (command.equals("h")) { // help command
+            this.help();
+
+        } else { // invalid command
             System.out.println("Please enter valid command");
         }
 
+        // formating gaps for easy readability
+        System.out.println();
+
         return continueProgram;
+
+    }
+
+    private void help() {
+        System.out.println("This Program contains information about F1 World Championship from 1950 - 2024");
+        System.out.println("\nThe following commands are available in this programm:");
+        System.out.println("1.  drivers                           - Lists all drivers in the database.");
+        System.out.println("2.  constructors                      - Lists all constructors (teams) in the database.");
+        System.out.println("3.  races                             - Lists all races in the database.");
+        System.out.println("4.  team_drivers <team_name>          - Lists all drivers for a given team.");
+        System.out.println("5.  driver_teams <driver_name>        - Lists all teams a given driver has driven.");
+        System.out.println("6.  circuits                          - Lists all race circuits in the database.");
+        System.out.println(
+                "7.  fastest_lap <driver_name>         - Displays the fastest lap times for a given driver.");
+        System.out.println(
+                "8.  top_drivers                       - Displays the top-performing drivers in the database.");
+        System.out.println(
+                "9.  search_driver <partial_name>      - Searches for a driver by a partial name.");
+        System.out.println(
+                "\nNote: commnands are in the format - COMMAND <VARIABLE>. Variables are only required for selected commands, see above.\n");
     }
 
 }
