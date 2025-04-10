236cee056f0c252676734d378292fb055f2f4008
charlesC <charles.chu9@gmail.com>
2025-04-10 15:19:34 -0500

    updated races

--- DatabaseLoader.java
+++ DatabaseLoader.java
@@ -17,7 +17,6 @@ public class DatabaseLoader {
     private static final String FILENAME = "f1.sql"; // create databse
 
     public static void main(String[] args) {
-        System.out.println("Loading database...");
         DatabaseLoader dataLoader = new DatabaseLoader();
         if (!dataLoader.loadData()) {
             System.out.println("Unable to load Database");
